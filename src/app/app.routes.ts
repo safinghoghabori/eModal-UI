df031f4684d44e190071351c04f879c95e8c2c0d
safinghoghabori <safinghoghabori65@gmail.com>
2024-11-21 15:24:51 +0530

    add login ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { CommonComponent } from './layout/common/common.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: CommonComponent,
-    children: [{ path: '', component: LandingPageComponent }],
+    children: [
+      { path: '', component: LandingPageComponent },
+      { path: 'login', component: LoginComponent },
+    ],
   },
 ];
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  isLoading: boolean = false;
+  errorMessage: string | null = null;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const credentials = {
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value,
+    };
+
+    this.isLoading = true;
+  }
+}
