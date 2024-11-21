35343f56cea709fb702146582543ee2a15da9d12
safinghoghabori <safinghoghabori65@gmail.com>
2024-11-21 22:26:56 +0530

    update header ui

--- src/app/features/dashboard/dashboard-header/dashboard-header.component.ts
+++ src/app/features/dashboard/dashboard-header/dashboard-header.component.ts
@@ -1,10 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { UserData } from '../../../auth/login/models/login.model';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { LocalStorageService } from '../../../core/services/local-storage.service';
 
 @Component({
   selector: 'dashboard-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard-header.component.html',
   styleUrl: './dashboard-header.component.css',
 })
-export class DashboardHeaderComponent {}
+export class DashboardHeaderComponent {
+  userData: UserData;
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private localStorageService: LocalStorageService
+  ) {
+    this.userData = this.localStorageService.getUserData();
+  }
+
+  getUserName(): string {
+    return this.userData.firstName + ' ' + this.userData.lastName;
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
