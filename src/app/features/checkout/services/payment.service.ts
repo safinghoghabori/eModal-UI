9159c2e5dd838fea8d202afe83771a9f8b5a200b
safinghoghabori <safinghoghabori65@gmail.com>
2024-11-24 19:30:04 +0530

    add bearer token in header

--- src/app/features/checkout/services/payment.service.ts
+++ src/app/features/checkout/services/payment.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ErrorHandlerService } from '../../../core/services/error-handler.service';
 import { Observable } from 'rxjs';
 import { PaymentResp, PaymentRequest } from '../models/checkout.model';
+import { LocalStorageService } from '../../../core/services/local-storage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,13 +11,19 @@ export class PaymentService {
   private apiUrl = 'https://localhost:7090/api/payments';
   constructor(
     private http: HttpClient,
-    private errorHandlingService: ErrorHandlerService
+    private localStorageService: LocalStorageService
   ) {}
 
   initiatePayment(paymentRequest: PaymentRequest): Observable<PaymentResp> {
     return this.http.post<PaymentResp>(
       `${this.apiUrl}/initiate`,
-      paymentRequest
+      paymentRequest,
+      {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.localStorageService.getToken()}`,
+        }),
+      }
     );
   }
 }
