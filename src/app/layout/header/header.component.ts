d95c454f06168639e319c4ec34bc7465e3eb8ec4
safinghoghabori <safinghoghabori65@gmail.com>
2024-11-25 10:33:48 +0530

    change landing page header ui

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +8,10 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  onRegisterClick(): void {
+    this.router.navigate(['/register']);
+  }
+}
